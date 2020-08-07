a3641e87966b5132a9f8318b572493aab873074c
ks6088ts <ks6088ts@gmail.com>
2020-08-07 23:00:14 +0900

    implement resolvers

--- pkg/gql/resolver.go
+++ pkg/gql/resolver.go
@@ -1,7 +1,12 @@
 package gql
 
+import "github.com/ks6088ts/sandbox-go/pkg/gql/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+// Resolver ...
+type Resolver struct {
+	todos []*model.Todo
+}
